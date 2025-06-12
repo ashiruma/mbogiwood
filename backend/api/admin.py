80b29c4df924d11862601847d6efe4fb7f0c1f85
ashiruma <ashirumaabala@gmail.com>
2025-06-12 14:14:11 +0300

    Create admin.py

---
+++ backend/api/admin.py
@@ -0,0 +1,6 @@
+# mbogiwood_backend/mbogiwood_api/admin.py
+
+from django.contrib import admin
+
+# Remove or comment out any previous attempts to import OTPAdminSite or modify admin.site.__class__
+# This file should remain simple.
